dfafcc9b618ace914f8dd69fb66a68a990eeed0f
Tomer Halali <tomerh@webtech-inv.co.il>
2023-07-24 20:04:25 +0300

    adding store name and changing the object

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -3,8 +3,20 @@ const Stores = require("../models/Stores");
 
 const getStoreInformation = async (req, res) => {
   const store = await Stores.find();
+  if (store) {
+    const storeInformation = {
+        aboutText: store[0].aboutText,
+        usefulLinks: store[0].usefulLinks,
+        openingHours: store[0].openingHours,
+        mediaLinks: store[0].mediaLinks,
+        information: store[0].information,
+        storeName: store[0].storeName
+    };
 
-  res.status(200).send(store[0]);
+    res.status(200).send(storeInformation);
+  } else {
+    res.sendStatus(404);
+  }
 };
 
 router.get("/info", getStoreInformation);
