ec125cd49a4428a27f588dab190ce001bf458798
Tomer Halali <tomerh@webtech-inv.co.il>
2023-08-13 17:03:57 +0300

    adding filter route

--- routes/routes/routes.js
+++ routes/routes/routes.js
@@ -2,10 +2,13 @@ const router = require("express").Router();
 const authController = require("../../controllers/authController");
 const sectionsController = require("../../controllers/sectionsController")
 const storeController = require("../../controllers/storeController")
+const filterController = require("../../controllers/filterController")
+
 
 router.use("/storeManagement", storeController);
 router.use("/authManagement", authController);
 router.use("/sectionsManagement", sectionsController);
+router.use("/filtersManagement", filterController);
 
 
 module.exports = router;
