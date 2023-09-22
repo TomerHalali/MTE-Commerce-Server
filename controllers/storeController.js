5ada3488d0c83957e1f69551b6f5385b95c851a2
Matan Balestra <matan.balestra@gmail.com>
2023-09-22 11:54:47 +0300

    add more info to the response getStoreInfo

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,22 +2,24 @@ let router = require("express").Router();
 const Stores = require("../models/Stores");
 
 const getStoreInformation = async (req, res) => {
-  const store = await Stores.find();
-  if (store) {
-    const storeInformation = {
-        aboutText: store[0].aboutText,
-        usefulLinks: store[0].usefulLinks,
-        openingHours: store[0].openingHours,
-        mediaLinks: store[0].mediaLinks,
-        information: store[0].information,
-        storeName: store[0].storeName,
-        ourServices: store[0].ourServices
-    };
+    const store = await Stores.find({});
+    if (store) {
+        const storeInformation = {
+            aboutText: store[0]._doc.aboutText,
+            usefulLinks: store[0]._doc.usefulLinks,
+            openingHours: store[0]._doc.openingHours,
+            mediaLinks: store[0]._doc.mediaLinks,
+            information: store[0]._doc.information,
+            storeName: store[0]._doc.storeName,
+            ourServices: store[0]._doc.ourServices,
+            headerAddsTexts: store[0]._doc.headerAddsTexts,
+            mainBannerUrl: store[0]._doc.mainBannerUrl
+        };
 
-    res.status(200).send(storeInformation);
-  } else {
-    res.sendStatus(404);
-  }
+        res.status(200).send(storeInformation);
+    } else {
+        res.sendStatus(404);
+    }
 };
 
 router.get("/info", getStoreInformation);
--- models/Stores.js
+++ models/Stores.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 const StoresSchema = new mongoose.Schema({
-  storeId: String,
-  information: Array,
-  usefulLinks: Array,
-  openingHours: Array,
-  aboutText: String,
-  mediaLinks: Array,
-  storeName: String,
-  ourServices: Array
+    storeId: String,
+    information: Array,
+    usefulLinks: Array,
+    openingHours: Array,
+    aboutText: String,
+    mediaLinks: Array,
+    storeName: String,
+    ourServices: Array,
+    headerAddsTexts: Array
 });
 
 module.exports = Stores = mongoose.model("Stores", StoresSchema);
